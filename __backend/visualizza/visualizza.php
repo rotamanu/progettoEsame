c2d820f6ec888d5ea6b9a91343ff0d764b293c7a
rotamano <rotamanuesperia@gmail.com>
2022-05-31 00:03:26 +0200

    ***

--- __backend/visualizza/visualizza.php
+++ __backend/visualizza/visualizza.php
@@ -8,41 +8,36 @@
     <link rel="stylesheet" href="../../style.css">
   </head>
   <body id="disegnobg">
-<?php
-$my_database=new mysqli("localhost", "root", "", "rotaspa");
-
-$stmt=$my_database->prepare("SELECT * FROM prodotti WHERE codice=?");
-$codice=$_POST['codice'];
-$stmt->bind_param('s', $codice);
-$stmt->execute();
-$result=$stmt->get_result();
-//var_dump($result->fetch_all());
-echo "<table border='1'>";
-if($row=$result->fetch_assoc()){
-
-  //variable with image path by database
-  //$img=realpath($row['foto']);
-  //$content = file_get_contents($img);
-  //header('Content-Type: image/jpg');
-  //echo $content;
-
-        echo "<tr>";
-        echo "<td>Numero matricola: ".$_SESSION['matricola_utente']."</td>";
-        echo "</tr>";
-        echo "<td><img src=".$row['disegno']." width=\"450px\" height=\"450px\"></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>";
+    <?php
+      $my_database=new mysqli("localhost", "root", "", "rotaspa");
+      $stmt=$my_database->prepare("SELECT * FROM prodotti WHERE codice=?");
+      $codice=$_POST['codice'];
+      $stmt->bind_param('s', $codice);
+      $stmt->execute();
+      $result=$stmt->get_result();
+      echo "<table border='6'>";
+      if($row=$result->fetch_assoc()){
+        //variable with image path by database
+        //$img=realpath($row['foto']);
+        //$content = file_get_contents($img);
+        //header('Content-Type: image/jpg');
+        //echo $content;
+        echo "<tr><td>codice     </td><td>data oggi    </td><td>scadenza   </td></tr>";
+        echo "<tr><td rowspan=\"9\"><img src=".$row['disegno']." width=\"450px\" height=\"450px\"></td><td rowspan=\"9\"><img src=".$row['foto']." width=\"450px\" height=\"450px\"></td></tr>";
+        echo "<tr><td>componente a     :</td></tr>";
+        echo "<tr><td>componente b     :</td></tr>";
+        echo "<tr><td>componente c     :</td></tr>";
+        echo "<tr><td>componente d     :</td</tr>";
+        echo "<tr><td>montato          </td></tr>";
+        echo "<tr><td>level            </td></tr>";
+        echo "<tr><td>ant              </td></tr>";
+        echo "<tr><td>matr             </td></tr>";
         //echo <form method="post" action="./index.html">
         //echo <h1>torna alla home</h1>
         //echo <button type="submit" name="login">indietro</button>
         //echo </form>
-        echo "</td>";
-        echo "</tr>";
-}
-echo "</table>";
-?>
-</body>
+      }
+    echo "</table>";
+    ?>
+  </body>
 </html>
