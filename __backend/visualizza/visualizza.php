db8c6dddff6f9d41b1466a9b03c6fb4f53f40f94
rotamano <rotamanuesperia@gmail.com>
2022-05-31 16:08:57 +0200

    css+foto etichetta

--- __backend/visualizza/visualizza.php
+++ __backend/visualizza/visualizza.php
@@ -15,23 +15,23 @@
       $stmt->bind_param('s', $codice);
       $stmt->execute();
       $result=$stmt->get_result();
-      echo "<table border='6'>";
+      echo "<table border='6' bgcolor='white'>";
       if($row=$result->fetch_assoc()){
         //variable with image path by database
         //$img=realpath($row['foto']);
         //$content = file_get_contents($img);
         //header('Content-Type: image/jpg');
         //echo $content;
-        echo "<tr><td>codice     </td><td>data oggi    </td><td>scadenza   </td></tr>";
+        echo "<tr><td>".$row['codice']."     </td><td>".$row['codice_esterno']."    </td><td>".$row['marca']."   </td></tr>";
         echo "<tr><td rowspan=\"9\"><img src=".$row['disegno']." width=\"450px\" height=\"450px\"></td><td rowspan=\"9\"><img src=".$row['foto']." width=\"450px\" height=\"450px\"></td></tr>";
-        echo "<tr><td>componente a     :</td></tr>";
-        echo "<tr><td>componente b     :</td></tr>";
-        echo "<tr><td>componente c     :</td></tr>";
-        echo "<tr><td>componente d     :</td</tr>";
-        echo "<tr><td>montato          </td></tr>";
-        echo "<tr><td>level            </td></tr>";
-        echo "<tr><td>ant              </td></tr>";
-        echo "<tr><td>matr             </td></tr>";
+        echo "<tr><td>".$row['componente_a']."</td></tr>";
+        echo "<tr><td>".$row['componente_b']."</td></tr>";
+        echo "<tr><td>".$row['componente_c']."</td></tr>";
+        echo "<tr><td>".$row['componente_d']."</td</tr>";
+        echo "<tr><td>".$row['montato_su']."</td></tr>";
+        echo "<tr><td>".$row['level']."</td></tr>";
+        echo "<tr><td>".$row['anteriore']."</td></tr>";
+        echo "<tr><td>".$_SESSION['matricola_utente']."</td></tr>";
         //echo <form method="post" action="./index.html">
         //echo <h1>torna alla home</h1>
         //echo <button type="submit" name="login">indietro</button>
