f55d2e0ba9de29ead311135cac5bb46ff9a69fd0
rotamano <rotamanuesperia@gmail.com>
2022-05-31 16:30:12 +0200

    +++

--- ___db/rotaspa.sql
+++ ___db/rotaspa.sql
@@ -48,17 +48,17 @@ CREATE TABLE IF NOT EXISTS `prodotti` (
 --
 
 INSERT INTO `prodotti` (`codice`, `codice_esterno`, `componente_a`, `componente_b`, `componente_c`, `componente_d`, `disegno`, `foto`, `montato_su`, `marca`, `level`, `anteriore`) VALUES
-('1111', '15468ab', '1', '1', '1', '1', '../../___disegni_tecnici_esame/disegno_1.jpg', '../../___disegni_tecnici_esame/disegno_12.jpg', 'SUV', 'Toyota', 'A', '1'),
-('2222', '15487bf', '2', '2', '2', '2', '../../___disegni_tecnici_esame/disegno_2.jpg', '../../___disegni_tecnici_esame/disegno_13.jpg', 'CAR', 'Ferrari', 'A', '1'),
-('3333', '15499cf', '3', '3', '3', '3', '../../___disegni_tecnici_esame/disegno_3.jpg', '../../___disegni_tecnici_esame/disegno_14.jpg', 'MONO', 'Porche', 'B', '0'),
-('4444', '65414we', '4', '4', '4', '4', '../../___disegni_tecnici_esame/disegno_4.jpg', '../../___disegni_tecnici_esame/disegno_15.jpg', 'MOTO', 'Bmw', 'B', '0'),
-('5555', '51465aa', '5', '5', '5', '5', '../../___disegni_tecnici_esame/disegno_5.jpg', '../../___disegni_tecnici_esame/disegno_16.jpg', 'AUTO', 'Bmw', 'B', '0'),
-('6666', '84198qq', '6', '6', '6', '6', '../../___disegni_tecnici_esame/disegno_6.jpg', '../../___disegni_tecnici_esame/disegno_17.jpg', 'MOTO', 'Audi', 'N', '0'),
-('7777', '65141fz', '7', '7', '7', '7', '../../___disegni_tecnici_esame/disegno_7.jpg', '../../___disegni_tecnici_esame/disegno_18.jpg', 'AUTO', 'Lancia', 'N', '1'),
-('8888', '74897ps', '8', '8', '8', '8', '../../___disegni_tecnici_esame/disegno_8.jpg', '../../___disegni_tecnici_esame/disegno_19.jpg', 'MONO', 'Fiat', 'N', '1'),
-('9999', '14981ld', '9', '9', '9', '9', '../../___disegni_tecnici_esame/disegno_9.jpg', '../../___disegni_tecnici_esame/disegno_20.jpg', 'PULL', 'Fiat', 'A', '1'),
-('1010', '74198br', '10', '10', '10', '10', '../../___disegni_tecnici_esame/disegno_10.jpg', '../../___disegni_tecnici_esame/disegno_21.jpg', 'MOTO', 'generico', 'A', '0'),
-('1110', '74987hs', '11', '11', '11', '11', '../../___disegni_tecnici_esame/disegno_11.jpg', '../../___disegni_tecnici_esame/disegno_22.jpg', 'MOTO', 'generico', 'A', '0');
+('1111', '15468ab', '1', '1', '1', '1', '../../___disegni_tecnici_esame/disegno_1.jpg', '../../___disegni_tecnici_esame/disegno_12.jpg', 'SUV', 'Toyota', 'A', 'front'),
+('2222', '15487bf', '2', '2', '2', '2', '../../___disegni_tecnici_esame/disegno_2.jpg', '../../___disegni_tecnici_esame/disegno_13.jpg', 'CAR', 'Ferrari', 'A', 'rear'),
+('3333', '15499cf', '3', '3', '3', '3', '../../___disegni_tecnici_esame/disegno_3.jpg', '../../___disegni_tecnici_esame/disegno_14.jpg', 'MONO', 'Porche', 'B', 'front'),
+('4444', '65414we', '4', '4', '4', '4', '../../___disegni_tecnici_esame/disegno_4.jpg', '../../___disegni_tecnici_esame/disegno_15.jpg', 'MOTO', 'Bmw', 'B', 'rear'),
+('5555', '51465aa', '5', '5', '5', '5', '../../___disegni_tecnici_esame/disegno_5.jpg', '../../___disegni_tecnici_esame/disegno_16.jpg', 'AUTO', 'Bmw', 'B', 'front'),
+('6666', '84198qq', '6', '6', '6', '6', '../../___disegni_tecnici_esame/disegno_6.jpg', '../../___disegni_tecnici_esame/disegno_17.jpg', 'MOTO', 'Audi', 'N', 'rear'),
+('7777', '65141fz', '7', '7', '7', '7', '../../___disegni_tecnici_esame/disegno_7.jpg', '../../___disegni_tecnici_esame/disegno_18.jpg', 'AUTO', 'Lancia', 'N', 'front'),
+('8888', '74897ps', '8', '8', '8', '8', '../../___disegni_tecnici_esame/disegno_8.jpg', '../../___disegni_tecnici_esame/disegno_19.jpg', 'MONO', 'Fiat', 'N', 'front'),
+('9999', '14981ld', '9', '9', '9', '9', '../../___disegni_tecnici_esame/disegno_9.jpg', '../../___disegni_tecnici_esame/disegno_20.jpg', 'PULL', 'Fiat', 'A', 'rear'),
+('1010', '74198br', '10', '10', '10', '10', '../../___disegni_tecnici_esame/disegno_10.jpg', '../../___disegni_tecnici_esame/disegno_21.jpg', 'MOTO', 'generico', 'A', 'rear'),
+('1110', '74987hs', '11', '11', '11', '11', '../../___disegni_tecnici_esame/disegno_11.jpg', '../../___disegni_tecnici_esame/disegno_22.jpg', 'MOTO', 'generico', 'A', 'front');
 
 -- --------------------------------------------------------
 
