9c1e646aa02080268ae17967b44267ef91086ca0
39342 <dr_lvr_mail@yahoo.it>
2022-06-13 22:12:31 +0200

    ***added dialog to delete

--- __backend/cancella/cancella.php
+++ __backend/cancella/cancella.php
@@ -1,28 +1,11 @@
 <?php
-
-$servername="localhost";
-$username="root";
-$password="";
-$db="rotaspa";
-$connessione=mysqli_connect($servername,$username,$password,$db);
-if(!$connessione){
-  die("Connessione fallita:".mysqli_connect_error());
-}
-else{
-  echo"CONNESSIONE ESEGUITA CON SUCCESSO";
-}
-if (isset($_POST['codice'])) ){
-  $codice= $_POST['codice'];
-  $sql="DELETE FROM prodotti WHERE codice=$codice";
-  if(mysqli_query($connessione, $sql)){
-    echo"record cancellato con successo";
-  }else{
-    echo "errore".$sql."<br>".mysqli_error($connessione);
-  }
-}
-else{
-  echo "codice non trovato";
-}
-
-mysqli_close($connessione);
+$my_database=new mysqli("localhost", "root", "", "rotaspa");
+$stmt=$my_database->prepare("DELETE FROM prodotti WHERE codice=?");
+$codice=$_POST['codice'];
+$stmt->bind_param('s', $codice);
+$stmt->execute();
+echo "<script>
+alert('Stai per essere redirezionato alla pagina principale');
+window.location.href='../../_admin/admin.html';
+</script>";
 ?>
