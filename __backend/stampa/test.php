6f0d53487a62efb0f18b20e98f9f853994899b28
39342 <dr_lvr_mail@yahoo.it>
2022-06-20 20:11:39 +0200

    ***

--- __backend/stampa/test.php
+++ __backend/stampa/test.php
@@ -1,6 +1,11 @@
 <?PHP
 session_start();
 require('code128.php');
+$day=date("d");
+$month=date("m");
+$year=date("Y");
+$data_produzione=$year."-".$month."-".$day;
+$data_di_scadenza=($year+5)."-".$month."-".$day;
 $pdf = new PDF_Label('L7163');
 $pdf->AddPage();
 $my_database=new mysqli("localhost", "root", "", "rotaspa");
@@ -13,7 +18,7 @@ $row=$result->fetch_assoc();
 // Print labels
 $num_etichette=$_POST['num_etichette'];
 for($i=1;$i<=$num_etichette;$i++) {
-    $text = sprintf("Cod. %s\nC.Es. %s\nMatr: %s\nMounted on: %s\n%s Marca: %s; %s", $row['codice'], $row['codice_esterno'], $_SESSION['matricola_utente'], $row['montato_su'], $row['anteriore'], $row['marca'], 'MADE IN ITALY');
+    $text = sprintf("Cod. %s\nC.Es. %s\nMatr: %s\nMounted on: %s\n%s Marca: %s; %s\n%s\nScade il: %s", $row['codice'], $row['codice_esterno'], $_SESSION['matricola_utente'], $row['montato_su'], $row['anteriore'], $row['marca'], 'MADE IN ITALY', $data_produzione, $data_di_scadenza);
     $pdf->Add_Label($text);
 }
 ob_start();
